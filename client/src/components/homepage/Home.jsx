8b1d43f94a0fdea0ba6213ab04cb51a03cf486f4
Rufus Roffie <jinbl12go@gmail.com>
2021-12-08 15:55:05 -0800

    built most components in homepage, ready to polish

--- client/src/components/homepage/Home.jsx
+++ client/src/components/homepage/Home.jsx
@@ -1,23 +1,43 @@
-import React from 'react';
+import React, {useState}from 'react';
 import { Routes, Route, Link } from "react-router-dom";
 import TopicsPage from '../topics/TopicsPage.jsx';
 import Chatroom from '../chatroom/Chatroom.jsx';
 import Topbar from './topbar/Topbar';
+import Menu from './menu/Menu';
 import "./home.scss"
 
-let Home = (props) => {
-
+const Home = (props) => {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className="homepage" id="homepage">
-      <Topbar/>
-      <div className="irrelavant">
-        <nav>
-          <Link to="/topics">Topics Page</Link>
-        </nav>
-        <nav>
-          <Link to="/chatroom">Chatroom Page</Link>
-        </nav>
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <div className="container">
+        <div className="left">
+          <div className="wrapper">
+            <h2>Our Mission</h2>
+            <h3>
+             enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </h3>
+            <h3>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </h3>
+            <nav>
+              <Link to="/topics">Topics Page</Link>
+            </nav>
+            <nav>
+              <Link to="/chatroom">Chatroom Page</Link>
+            </nav>
+          </div>
+        </div>
+        <div className="right">
+          <div className="imgContainer">
+            <img src="assets/childCare.jpg" alt=""></img>
+          </div>
+        </div>
+
       </div>
+
     </div>
   )
 }
