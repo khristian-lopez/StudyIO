099652da1281b147822b8df221c621f2c8763c3f
Rufus Roffie <jinbl12go@gmail.com>
2021-12-08 10:38:46 -0800

    added style-loader/css-loader/sass-loader to webpack, now i can use sass and import the style directly

--- client/src/components/homepage/Home.jsx
+++ client/src/components/homepage/Home.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import { Routes, Route, Link } from "react-router-dom";
 import TopicsPage from '../topics/TopicsPage.jsx';
 import Chatroom from '../chatroom/Chatroom.jsx';
+import "./home.scss"
 
 let Home = (props) => {
 
   return (
-    <div>
+    <div className="homepage" id="homepage">
       <nav>
         <Link to="/topics">Topics Page</Link>
       </nav>
+      <div className="test">
+      </div>
       <nav>
         <Link to="/chatroom">Chatroom Page</Link>
       </nav>
