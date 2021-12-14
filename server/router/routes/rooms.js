1b098e8844655bdc1e7921cf2046e8376dceae5d
Brian Lowther <merchanthagen@gmail.com>
2021-12-13 16:52:24 -0800

    fixed an api route

--- server/router/routes/rooms.js
+++ server/router/routes/rooms.js
@@ -15,9 +15,11 @@ router.get('/', (req, res) => {
 
 //Search by name for specific room
 router.get('/name/:name', (req, res) => { // request body param search_value = req.params.name
-  console.log(url + `/rooms/search?search_value=${req.params.name}`)
-  axios.get(url + `/rooms/search?search_value=${req.params.name}`)
-    .then((data) => { res.send(data.data).status(200); })
+  console.log(req.params.name)
+  axios.post(url + '/rooms/search', {"search_value": req.params.name})
+    .then((data) => { res.send(data.data).status(200)
+       console.log(data.data)
+    })
     .catch((err) => res.send(err).status(500));
 });
 
