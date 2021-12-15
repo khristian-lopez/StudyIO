9a28b4ecfbbf265c8219a50840245890d67dc599
Brian Lowther <merchanthagen@gmail.com>
2021-12-15 08:04:52 -0800

    cleaned up dates in events

--- client/src/components/chatroom/SingleEventOrGoal.jsx
+++ client/src/components/chatroom/SingleEventOrGoal.jsx
@@ -7,11 +7,13 @@ const individualEventOrGoalSx = {
 }
 
 let SingleEventOrGoal = (props) => {
+  const date1 = new Date(props.content.event_date).toLocaleDateString('en-US')
+  const date2 = new Date(props.content.event_date).toString().slice(0,15)
 
   return (
     <div style={individualEventOrGoalSx}>
       <span>{props.content.name}</span>
-      <span>{props.content.event_date}</span>
+      <span>{props.content.event_date ? date2 : ''}</span>
       {/* {props.content.time ? <span>{props.content.time}</span> : null}
       {props.content.date ? <span>{props.content.date}</span> : null} */}
     </div>
