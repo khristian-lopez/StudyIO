357ad5f38f4f1ca878963c04711b4ac60d8396c9
matthewnguyen <itsmatthewnguyen@gmail.com>
2021-12-17 11:31:51 -0800

    fixing unnecessary white space between code

--- client/src/components/topics/TopicsPage.jsx
+++ client/src/components/topics/TopicsPage.jsx
@@ -94,7 +94,6 @@ const TopicsPage = (props) => {
                 setCurrentTopicId={setCurrentTopicId}
                 openModal={openModal}
                 open={open}
-                
               />
             </Box>
           ))}
